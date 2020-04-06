a4e50f3290028e0838e93c3d411561ec355133a6
Nadel1 <nataliapio@yahoo.de>
2020-04-06 17:30:38 +0200

    added placeholder for gasstation, set up basic ui

---
+++ Assets/Scripts/RefuelShip.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RefuelShip : MonoBehaviour
+{
+    public TextMeshProUGUI welcomeText;
+    public TextMeshProUGUI serviceText;
+
+    private void Start()
+    {
+        welcomeText.text = "Welcome to the Gasstation!";
+        serviceText.text = "What can we do for you?\n Refuel: right arrow";
+        welcomeText.gameObject.SetActive(false);
+        serviceText.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (welcomeText.IsActive())
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerShip")
+        {
+            welcomeText.gameObject.SetActive(true);
+            serviceText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PlayerShip")
+        {
+            welcomeText.gameObject.SetActive(false);
+            serviceText.gameObject.SetActive(false);
+        }
+    }
+   
+}
